afd36bdd677ff63f1e02221870ea423317ff5851
THOTA SREENIDHI <sreenidhithota12@gmail.com>
2026-07-27 21:35:42 +0530

    Time: 10 ms (38.93%), Space: 47.2 MB (25.91%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.java
+++ 1154-day-of-the-year/1154-day-of-the-year.java
@@ -7,7 +7,9 @@ class Solution {
         int day = Integer.parseInt(parts[2]);
         int[] prefixDays = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
         int ans = prefixDays[month - 1] + day;
-        if(((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) && month>2) ans++;
+        if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+            if(month > 2) ans++;
+        }
         return ans;
     }
 }
