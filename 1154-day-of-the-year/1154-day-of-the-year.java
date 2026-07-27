903bf79ced7045c81384147d4b6dcdae36ab78ab
THOTA SREENIDHI <sreenidhithota12@gmail.com>
2026-07-27 21:43:55 +0530

    Time: 10 ms (38.93%), Space: 47.1 MB (45.2%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.java
+++ 1154-day-of-the-year/1154-day-of-the-year.java
@@ -1,5 +1,7 @@
 class Solution {
-    
+    boolean isLeapYear(int year) {
+    return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
     public int dayOfYear(String date) {
         String[] parts = date.split("-");
         int year = Integer.parseInt(parts[0]);
@@ -7,8 +9,8 @@ class Solution {
         int day = Integer.parseInt(parts[2]);
         int[] prefixDays = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
         int ans = prefixDays[month - 1] + day;
-        if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
-            if(month > 2) ans++;
+        if(isLeapYear(year) && month > 2 ) {
+             ans++;
         }
         return ans;
     }
