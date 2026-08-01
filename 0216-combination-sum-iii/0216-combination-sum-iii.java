2733dbf85c75277f7c4cc189e5e4ba20d09f3964
THOTA SREENIDHI <sreenidhithota12@gmail.com>
2026-08-01 12:03:25 +0530

    Time: 0 ms (100%), Space: 42.3 MB (82.92%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,24 +1,25 @@
 class Solution {
-    void find(int[] arr, int n,List<List<Integer>> sol,int ind, List<Integer> ans,int k){
+    void find(int n,List<List<Integer>> sol,int ind, List<Integer> ans,int k){
         if(ans.size() == k){
             if(n==0){
-            sol.add( new ArrayList<>(ans));}
+            sol.add( new ArrayList<>(ans));
+            }
             return;
         }
-        for(int i = ind ;i < arr.length ; i++){
-            if(arr[i] > n) break;
-            ans.add(arr[i]);
-            find(arr,n-arr[i],sol,i+1,ans,k);
+        for(int i = ind ;i < 10 ; i++){
+            if(i > n) break;
+            ans.add(i);
+            find(n-i,sol,i+1,ans,k);
             ans.remove(ans.size() - 1);
         }
     }
     public List<List<Integer>> combinationSum3(int k, int n) {
-        int[] arr = {1,2,3,4,5,6,7,8,9};
+        
         int mini = k*(k+1)/2;
         if(n < mini) return new ArrayList<>();
         List<List<Integer>> sol = new ArrayList<>();
         List<Integer> ans = new ArrayList<>();
-        find(arr,n,sol,0,ans,k);
+        find(n,sol,1,ans,k);
         return sol;
     }
 }
