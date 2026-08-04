4253e9b859fa18f27d9a2a7a6bd341e2b4dae746
THOTA SREENIDHI <sreenidhithota12@gmail.com>
2026-08-04 22:39:53 +0530

    Time: 2 ms (91.99%), Space: 46.7 MB (67.71%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -4,15 +4,18 @@ class Solution {
         int mini=Integer.MAX_VALUE ;
         int maxi=Integer.MIN_VALUE ;
         List<Integer> sol = new ArrayList<>();
-        List<Integer> pre = new ArrayList<>();
         for(int x:nums){
             mini=Math.min(mini,x);
-            pre.add(x);
+            
             maxi=Math.max(maxi,x);
         }
         if(maxi - mini + 1 == s) return new ArrayList<>();
+        int[] found = new int [maxi+1];
+        for(int x : nums){
+            found[x]++;
+        }
         for(int j=mini+1;j<maxi;j++){
-            if (!pre.contains(j)) {
+            if (found[j]==0) {
                 sol.add(j);
             }
         }
