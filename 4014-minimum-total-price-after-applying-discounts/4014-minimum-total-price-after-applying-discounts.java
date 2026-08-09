f8b9d1c2095436d50b413ff482d8faf31ed36d1a
THOTA SREENIDHI <sreenidhithota12@gmail.com>
2026-08-10 01:33:18 +0530

    Time: 44 ms (100%), Space: 146 MB (100%) - LeetHub

---
+++ 4014-minimum-total-price-after-applying-discounts/4014-minimum-total-price-after-applying-discounts.java
@@ -0,0 +1,20 @@
+class Solution {
+    public double minPrice(int[] prices, int[] discounts) {
+        Arrays.sort(prices);
+        Arrays.sort(discounts);
+        int n = prices.length;
+        int m = discounts.length;
+        int i=n-1,j=m-1;
+        double ans = 0;
+        while(i >=0 && j >=0){
+            ans += (prices[i] * (100 - discounts[j])) / 100.0;
+            i--;
+            j--;
+        }
+        while(i>=0){
+            ans+=prices[i];
+            i--;
+        }
+        return ans;
+    }
+}
