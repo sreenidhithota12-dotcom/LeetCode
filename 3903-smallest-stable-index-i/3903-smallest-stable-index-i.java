e9da6931f2deed8b5c6c0a4031d3de99ad75377a
THOTA SREENIDHI <sreenidhithota12@gmail.com>
2026-09-04 23:21:18 +0530

    Time: 1 ms (99.79%), Space: 46.4 MB (49.36%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -6,7 +6,6 @@ class Solution {
         for(int i=n-2;i>=0;i--){
             suf[i]=Math.min(nums[i],suf[i+1]);
         }
-        int ans=-1;
         int pre=Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
             pre=Math.max(pre,nums[i]);
